7545ab0c501ad5fa46c97adab54633e1a89caca5
Guillermo Gutierrez <ggutierrezm@users.noreply.github.com>
2019-07-23 06:50:15 +0200

    Update register.php
    I have corrected a typo, that affect the register page, preventing new user to be able of register new accounts

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION)) { session_start(); }
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Own Files/title>
+  <title>Own Files</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
