d14a830285658a329b6618e6e28d4a891ce33fd1
Guillermo Gutierrez <ggutierrezm@users.noreply.github.com>
2019-07-24 11:48:53 +0200

    Update menu.php

--- contents/menu.php
+++ contents/menu.php
@@ -1,8 +1,10 @@
-<!--24 Jul 2019
-Copyright (c) 2019, Guillermo Gutierrez Morote
-Released under the GPL license
-http://www.gnu.org/copyleft/gpl.html
--->
+
+<?php
+// 24 Jul 2019
+// Copyright (c) 2019, Guillermo Gutierrez Morote
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+?>
 <ul>
 	<li><a href="main.php?target=upload"><span class="glyphicon glyphicon-cloud-upload"></span> Upload</a></li>
 	<li><a href="main.php?target=myfiles"><span class="glyphicon glyphicon-cloud-download"></span> My files</a></li>
